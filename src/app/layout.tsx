2635b63c6ff619e3dd6e792e56cb8d9813cda708
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-03-17 16:05:57 +0100

    feat(drizzle): drizzle setup and initialization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Icons } from '@/components/icons'
+import SearchBar from '@/components/search-bar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -57,7 +58,7 @@ export default function RootLayout({
               </p>
 
               <div className="mx-auto mt-16 w-full max-w-2xl flex flex-col">
-                {/* <SearchBar /> */}
+                <SearchBar />
 
                 {children}
               </div>
---
+++ src/app/search/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from 'next/navigation'
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const query = searchParams.query
+
+  if (Array.isArray(query) || !query) {
+    return redirect('/')
+  }
+}
